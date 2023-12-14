11b65bdc97f797d62f7fdef8f8e0f60918fc11e4
reddevill007 <shivampandey2900@gmail.com>
2023-12-14 16:01:56 +0530

    updated function

--- main.py
+++ main.py
@@ -51,7 +51,13 @@ class ImageGeneratorApp:
         output = replicate.run(
             "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
             input={
-                "prompt": prompt
+                "width": 768,
+                "height": 768,
+                "prompt": prompt,
+                "scheduler": "DPMSolverMultistep",
+                "num_outputs": 1,
+                "guidance_scale": 7.5,
+                "num_inference_steps": 50
             }
         )
 
